2001fdd7e7fd4175540b99915073b4feaac183c3
XUANHAO <438164432@QQ.COM>
2021-08-19 13:54:01 +1200

    123

--- feedback/feedback.js
+++ feedback/feedback.js
@@ -3,26 +3,64 @@
 var http = require('http')
 var fs = require('fs')
 var template = require('art-template')
+var url = require('url')
+
+
+
+// var pathName=parseObj.pathname
+var  comments=[{
+    name:'howard',
+    message:'good day',
+    date:'2015/15/08'
+},
+{
+    name:'howard',
+    message:'good day',
+    date:'2015/15/08'
+},
+{
+    name:'howard',
+    message:'good day',
+    date:'2015/15/08'
+}]
 http.createServer(
-    function(req, res){
-var url=req.url
-if(url==='/'){
+    (req, res)=>{
+        var parseObj = url.parse(req.url,true)
+
+        // 单独获取不包含查询字符串的路径部分（该路径不包含 ? 之后的内容）
+        var pathname = parseObj.pathname
+if(pathname==='/'){
     fs.readFile('./view/index.html',function(err,data){
         if(err){
         return   res.end('404 not found')
         }
-        res.end(data)
+        var htmlstr=template.render(data.toString(),{comments:comments})//为htmlstr声明变量  render意思是渲染
+        res.end(htmlstr)
     })
 
-}else if(url.startsWith('/public')){
-    console.log(url)
-    fs.readFile('.'+url,(err,data)=>{
-        if(err){
-            res.end(err)
-        }
-        res.end(data)
-    })
-}else if(url==='/post'){
+}else if(pathname==='/pinglun'){
+    
+    //拿到数据 parseObj.query
+    //生成日期 加入到数组中
+    //让用户跳转到首页
+    const comment = parseObj.query
+    comment.date='2021/5/8'
+    comments.push(comment)
+    // 服务端这个时候已经把数据存储好了，接下来就是让用户重新请求 / 首页，就可以看到最新的留言内容了
+
+      // 如何通过服务器让客户端重定向？
+      //    1. 状态码设置为 302 临时重定向
+      //        statusCode
+      //    2. 在响应头中通过 Location 告诉客户端往哪儿重定向
+      //        setHeader
+      // 如果客户端发现收到服务器的响应的状态码是 302 就会自动去响应头中找 Location ，然后对该地址发起新的请求
+      // 所以你就能看到客户端自动跳转了
+      res.statusCode = 302
+      res.setHeader('Location', '/')
+      res.end()
+}
+
+else if(pathname==='/post'){
     fs.readFile('./view/post.html',(err,data)=>{
         console.log(url)
         if(err){
@@ -31,7 +69,15 @@ if(url==='/'){
         }
         res.end(data)
     })
-}else{
+}else if(pathname.startsWith('/public')){//把请求路径声明为一个变量url
+    console.log(pathname)
+    fs.readFile('.'+pathname,(err,data)=>{//html里每个样式 脚本 都会触发请求，这里根据路径变量 读取数据
+        if(err){
+            res.end(err)
+        }
+        res.end(data)
+    })
+}else{//其他路径请求访问 会显示404
     fs.readFile('./view/404.html',(err,data)=>{
         if(err){
             return res.end('404 not found')
